472acc7a5a42b4ac3f8ec2359b166aa2107e67cb
PRAFUL KUMAR <96467257+prafulk9155@users.noreply.github.com>
2022-05-11 13:50:16 +0530

    Add files via upload

---
+++ src/Assignment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Assignment extends Component {
+  render() {
+    return (
+      <div id="root">
+      <p> I am learning React. My life is getting better.</p></div>
+    );
+  }
+}
+
+export default Assignment;
